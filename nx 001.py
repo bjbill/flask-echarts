b4afe80155ac9a4f12508e85b3c8d7eaaeb92103
bjbill <sal12345@sina.com>
2018-03-12 22:47:32 +0800

    home

--- nx 001.py
+++ nx 001.py
@@ -15,31 +15,52 @@ cu.execute(sql1)
 r1 = cu.fetchall()
 
 
-sql2 = ("SELECT invested_company FROM inverst WHERE (company_stat != '吊销，未注销' "
-       "and company_stat != '吊销' and company_stat != '注销' )"
-       " EXCEPT SELECT COMPANY_NAME"
-       " FROM baseinfo")
+# sql2 = ("SELECT invested_company FROM inverst WHERE (company_stat != '吊销，未注销' "
+#       "and company_stat != '吊销' and company_stat != '注销' )"
+#       " EXCEPT SELECT COMPANY_NAME"
+#       " FROM baseinfo"
+#        )
+
+sql2 = ("SELECT baseinfo.company_name as holder,"
+        "inverst.invested_company as inc FROM inverst, baseinfo "
+        "WHERE (inverst.holder_org_id = baseinfo.organization_code)"
+)
 cu.execute(sql2)
 r2 = cu.fetchall()
 
-i = 0
+
 # 将baseinfo中的公司加入图中
 for cname, cid in r1:
 
-   G.add_node(i, cname=cname, cid=cid)
-   i = i + 1
-
-# 继续，将inverst中没有查询的公司（最末端的公司）加入图中
-for cname in r2:
-
-    G.add_node(i, cname=cname, cid="")
-    i = i + 1
+   G.add_node(cname, cid=cid)
 
 
-print(G.nodes(data=True))
+# 将inverst中关系加入图中
+for holder, inc in r2:
+
+    G.add_edge(holder, inc)
+
+print("打印G的节点list，带附加属性./n %s" %G.nodes(data=True))
+print(G.nodes())
+print(G.edges())
+print('节点数:%s，边数:%s' %(G.number_of_nodes(), G.number_of_edges()))
+print('节点数:{}，边数:{}'.format(G.number_of_nodes(), G.number_of_edges()))
+# '节点0的邻居节点：
+g = nx.neighbors(G,'国家能源投资集团有限责任公司')
+print([x for x in g])
+
+
+print(nx.degree(G))
+
 print(G.node)
-print(G.node[0])
-print(G.node[0]['cname'])
+#print(G.node[''])
+#print(G.node['']['cid']) # ['文本'] 因为直接用文本作为节点名称
 
 #nx.draw(G, data=True)
 #plt.show()
+
+plt.figure() #创建一幅图
+# nx.draw(G, node_color='y', with_labels=True, node_size=10)
+#node_color='y'表示绘制节点的颜色为黄色，默认为红色;with_labels=True使节点上显示节点的名字，默认为False;node_size设置节点大小，默认为300
+nx.draw(G, node_color='y', node_size=10)
+plt.show()
---
+++ nx-sh.py
@@ -0,0 +1,51 @@
+﻿#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@version: v1.0
+@author: zz
+@license: Apache Licence 
+@contact: 
+@site: 
+@software: PyCharm
+@file: nx-sh.py
+@time: 2018/3/9 15:40
+"""
+
+import networkx as nx
+import csv
+from networkx.readwrite import json_graph
+from pyecharts import Graph
+
+g = nx.Graph()
+
+
+with open('tyc-SH01-node.csv', newline='', encoding='utf-8') as csvfile: # `with` statement available in 2.5+
+    # csv.DictReader uses first line in file for column headings by default;
+    # in version 3.6: Returned rows are now of type OrderedDict.
+    dr = csv.DictReader(csvfile) # comma is default delimiter
+#   to_db = [(i['col1'], i['col2']) for i in dr]
+    for row in dr:
+        g.add_node(row['Id'], name=row['Label'])
+
+with open('tyc-SH01-eg.csv', newline='', encoding='utf-8') as csvfile1: # `with` statement available in 2.5+
+    # csv.DictReader uses first line in file for column headings by default;
+    # in version 3.6: Returned rows are now of type OrderedDict. Source,Target
+    dr = csv.DictReader(csvfile1) # comma is default delimiter
+#   to_db = [(i['col1'], i['col2']) for i in dr]
+    for row in dr:
+        g.add_edge(row['Source'], row['Target'])
+
+
+g_data = json_graph.node_link_data(g)
+#print(g_data)
+#eg = Graph('公司结构图')
+#eg.add('SHENHUA', nodes=g_data['nodes'], links=g_data['links'])
+#eg.render()
+
+graph = Graph("公司关系图", width=1200, height=600)
+graph.add("SHENHUA", nodes=g_data['nodes'], links=g_data['links'], label_pos="right",
+          graph_repulsion=50, is_legend_show=False, edgeSymbol=['circle', 'arrow'],graph_edge_symbolsize=[4, 10],
+          line_curve=0, label_text_color=None)
+graph.render()
